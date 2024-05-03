060e3fc94e77b90e24b4df130216fc84db063206
Gustavo <gualex89@gmail.com>
2024-05-03 19:12:06 -0400

    agrega whatsapp al correo

--- resources/views/emails/test.blade.php
+++ resources/views/emails/test.blade.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
 <html>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
 <head>
     <title>Gracias por tu Compra</title>
 </head>
@@ -8,7 +10,11 @@
     <img src="{{ asset('images/logo/logo_black.png') }}" alt="Logo de Makena">
     <h1>¡Tu compra se procesó de forma correcta!</h1>
     <h2>Tu numero de orden es: MK-{{ $orden }}</h2>
-    <h2>Este es un correo electrónico de prueba enviado desde Laravel.</h2>
-    <p>¡Espero que estés teniendo un buen día!</p>
+    <h2>Si tienes alguna duda, comunicate con nosotros a nuestro Whatsapp</h2>
+    <a href="http://wa.me/541122444188"><i class="fab fa-whatsapp"></i></a>
+    <h3>Este es un correo automático, por favor no responda.</h2>
+        
+       
+   
 </body>
 </html>
