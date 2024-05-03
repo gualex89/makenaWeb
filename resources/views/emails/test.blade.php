3751f21b58eeaa08a603f32af56ec2c4c887a3a7
Gustavo <gualex89@gmail.com>
2024-05-03 17:38:48 -0400

    prueba de correo

--- resources/views/emails/test.blade.php
+++ resources/views/emails/test.blade.php
@@ -4,7 +4,7 @@
     <title>Prueba de correo electrónico</title>
 </head>
 <body>
-    <img src="{{ asset('images/logo/logomakena.png') }}" alt="Logo de Makena">
+    
     <h1>¡Hola!</h1>
     <p>Este es un correo electrónico de prueba enviado desde Laravel.</p>
     <p>¡Espero que estés teniendo un buen día!</p>
