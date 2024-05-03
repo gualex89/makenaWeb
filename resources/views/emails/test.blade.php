d06f4a22b8bda53ff7d78c738811b0c051741599
Gustavo <gualex89@gmail.com>
2024-05-03 19:14:38 -0400

    fix a link

--- resources/views/emails/test.blade.php
+++ resources/views/emails/test.blade.php
@@ -10,8 +10,8 @@
     <img src="{{ asset('images/logo/logo_black.png') }}" alt="Logo de Makena">
     <h1>¡Tu compra se procesó de forma correcta!</h1>
     <h2>Tu numero de orden es: MK-{{ $orden }}</h2>
-    <h2>Si tienes alguna duda, comunicate con nosotros a nuestro Whatsapp</h2>
-    <a href="http://wa.me/541122444188"></a>
+    <h2>Si tienes alguna duda, comunicate con nosotros a nuestro<a href="http://wa.me/541122444188">Whatsapp</a> </h2>
+    
     <h3>Este es un correo automático, por favor no responda.</h2>
         
        
