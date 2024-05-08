fd442b6d2709912795853d6d75ea2f23fcb2ef92
Gustavo <gualex89@gmail.com>
2024-05-08 19:49:06 -0400

    fix orden

--- resources/views/emails/graciasEmail.blade.php
+++ resources/views/emails/graciasEmail.blade.php
@@ -9,7 +9,7 @@
 
     <img src="{{ asset('images/logo/logo_black.png') }}" alt="Logo de Makena">
     <h1>¡Tu compra se procesó de forma correcta!</h1>
-    <h2>Tu numero de orden es: MK-{{ $orden }}</h2>
+    <h2>Tu numero de orden es: MK-{{ $apiExternalReference }}</h2>
     <h3>id de pago: {{ $payment_id }}</h3>
     <h2>Si tenés alguna duda, comunicate con nosotros a nuestro <a href="http://wa.me/541122444188">Whatsapp</a> </h2>
     
