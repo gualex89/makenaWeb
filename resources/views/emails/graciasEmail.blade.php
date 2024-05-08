e7539f9ce2102c2770aef482ecf4392607a36864
Gustavo <gualex89@gmail.com>
2024-05-08 18:22:05 -0400

    prueba de contenido de email

--- resources/views/emails/graciasEmail.blade.php
+++ resources/views/emails/graciasEmail.blade.php
@@ -10,7 +10,7 @@
     <img src="{{ asset('images/logo/logo_black.png') }}" alt="Logo de Makena">
     <h1>¡Tu compra se procesó de forma correcta!</h1>
     <h2>Tu numero de orden es: MK-{{ $orden }}</h2>
-    <h3>id de pago: {{ $payment_id }}</h3>
+    <h3>id de pago: {{ $payment_id->documento, $payment_id->nombre }}</h3>
     <h2>Si tenés alguna duda, comunicate con nosotros a nuestro <a href="http://wa.me/541122444188">Whatsapp</a> </h2>
     
     <h3>Este es un correo automático, por favor no responda.</h2>
